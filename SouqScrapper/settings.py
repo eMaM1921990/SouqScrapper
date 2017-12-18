f1179d38ec09014ff55cc01473c201adea270279
ahmed_elemam <emam151987@gmail.com>
2017-12-19 00:28:01 +0200

    postgres

--- SouqScrapper/settings.py
+++ SouqScrapper/settings.py
@@ -87,9 +87,18 @@ DATABASES = {
     #         'PORT': '3306',
     #     }
 
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'souq',
+        'USER': 'postgres',
+        'PASSWORD': 'souq_admin',
+        'HOST': 'localhost',  # '127.0.0.1'
+        'PORT': '5432',  # '5556'
     }
 }
 
